82a87f5ba8eb06d02bb870fe75238aa725d8f822
Gant <GantMan@gmail.com>
2019-09-22 16:41:33 -0500

    stripe button

--- src/Components/ModelConverter.js
+++ src/Components/ModelConverter.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../App.css'
 import { withAuthenticator } from 'aws-amplify-react'
+import StripeBtn from './StripeButton'
 
 function MC() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>YOU LOGGED IN!</h1>
+        <StripeBtn />
       </header>
     </div>
   )
---
+++ src/Components/StripeButton.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
+const stripeBtn = () => {
+  const publishableKey = 'pk_test_LNFLm8rQEiYilbbqhXUX8Ogf00025ngOE8'
+
+  const onToken = token => {
+    const body = {
+      amount: 999,
+      token: token
+    }
+    axios
+      .post('http://localhost:8000/payment', body)
+      .then(response => {
+        console.log(response)
+        alert('Payment Success')
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error)
+        alert('Payment Error')
+      })
+  }
+  return (
+    <StripeCheckout
+      label="Buy Model Conversions" //Component button text
+      name="Infinite Red, Inc." //Modal Header
+      description="Purchase 12 model conversions"
+      panelLabel="12 Conversions" //Submit button in modal
+      amount={999} //Amount in cents $9.99
+      token={onToken}
+      currency="USD"
+      alipay // accept Alipay (default false)
+      bitcoin // accept Bitcoins (default false)
+      stripeKey={publishableKey}
+      image="https://i.imgur.com/PieUY1f.jpg" //Pop-in header image
+      billingAddress={false}
+    />
+  )
+}
+export default stripeBtn
